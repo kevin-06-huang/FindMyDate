babfd853aaecd3bc3983e064028cb4b944a1b7c5
Kevin Huang <kevincrazykid@gmail.com>
2017-09-04 06:51:08 -0700

    everything still working; implementing tab layout in main activity to support a new card fragment

--- app/src/main/java/com/example/findmydate/CardFragment.java
+++ app/src/main/java/com/example/findmydate/CardFragment.java
@@ -1,8 +1,51 @@
 package com.example.findmydate;
 
+import com.mindorks.placeholderview.SwipePlaceHolderView;
+import android.content.Context;
+import android.app.Fragment;
+import android.os.Bundle;
 /**
  * Created by kevincrazykid on 30/08/17.
  */
 
-public class CardFragment {
+public class CardFragment extends Fragment {
+
+    private SwipePlaceHolderView mSwipeView;
+    private Context mContext;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+      //  setContentView(R.layout.activity_main);
+    /*
+        mSwipeView = (SwipePlaceHolderView)findViewById(R.id.swipeView);
+        mContext = getApplicationContext();
+
+        mSwipeView.getBuilder()
+                .setDisplayViewCount(3)
+                .setSwipeDecor(new SwipeDecor()
+                        .setPaddingTop(20)
+                        .setRelativeScale(0.01f)
+                        .setSwipeInMsgLayoutId(R.layout.tinder_swipe_in_msg_view)
+                        .setSwipeOutMsgLayoutId(R.layout.tinder_swipe_out_msg_view));
+
+
+        for(Profile profile : Utils.loadProfiles(this.getApplicationContext())){
+            mSwipeView.addView(new TinderCard(mContext, profile, mSwipeView));
+        }
+
+        findViewById(R.id.rejectBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSwipeView.doSwipe(false);
+            }
+        });
+
+        findViewById(R.id.acceptBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSwipeView.doSwipe(true);
+            }
+        });*/
+    }
 }
